4540b027242bfb0b30708d0eed51b0c3f09a839a
Sasikumar JP <jpsasi@gmail.com>
2020-05-11 11:30:51 +0530

    redundant Provider code removed

--- lib/pages/content_page.dart
+++ lib/pages/content_page.dart
@@ -1,46 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:prime_demo/pages/counter_page.dart';
 import 'package:prime_demo/pages/favorite_primes_page.dart';
-import 'package:prime_demo/store/counter_store.dart';
-import 'package:provider/provider.dart';
 
 class ContentPage extends StatelessWidget {
   const ContentPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<CounterStore>(
-      create: (_) => CounterStore(),
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('State Management'),
-        ),
-        body: ListView(
-          children: <Widget>[
-            Card(
-              child: ListTile(
-                title: Text('Counter Demo'),
-                trailing: Icon(Icons.keyboard_arrow_right),
-                onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                    builder: (_) => CounterPage(),
-                  ));
-                },
-              ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('State Management'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Card(
+            child: ListTile(
+              title: Text('Counter Demo'),
+              trailing: Icon(Icons.keyboard_arrow_right),
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (_) => CounterPage(),
+                ));
+              },
             ),
-            Card(
-              child: ListTile(
-                title: Text('Favorite Primes'),
-                trailing: Icon(Icons.keyboard_arrow_right),
-                onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                    builder: (_) => FavoritePrimesPage(),
-                  ));
-                },
-              ),
-            )
-          ],
-        ),
+          ),
+          Card(
+            child: ListTile(
+              title: Text('Favorite Primes'),
+              trailing: Icon(Icons.keyboard_arrow_right),
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (_) => FavoritePrimesPage(),
+                ));
+              },
+            ),
+          )
+        ],
       ),
     );
   }
